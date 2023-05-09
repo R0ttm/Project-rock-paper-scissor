c3e0f4e9187d8c6699e55cec9aa627c4c11b8407
R0ttm <r0t7.mi@gmail.com>
2023-05-09 01:46:49 -0400

    feat: function game has been implemented in script file

--- script.js
+++ script.js
@@ -9,26 +9,48 @@ function getComputerChoice(){
 function playRound(playerSelection, ComputerSelection){
     if(playerSelection == "Rock" && ComputerSelection == "Paper")
         return "You Lose! Paper beats Rock";
-    if(playerSelection == "Rock" && ComputerSelection == "Scissors")
+    else if(playerSelection == "Rock" && ComputerSelection == "Scissors")
         return "You Win! Rock beats Scissors";
-    if(playerSelection == "Rock" && ComputerSelection == "Rock")
+    else if(playerSelection == "Rock" && ComputerSelection == "Rock")
         return "even";
 
     if(playerSelection == "Paper" && ComputerSelection == "Rock")
         return "You Win! Paper beats Rock";
-    if(playerSelection == "Paper" && ComputerSelection == "Scissors")
+    else if(playerSelection == "Paper" && ComputerSelection == "Scissors")
         return "You Lose! Scissors beats Paper";
-    if(playerSelection == "Paper" && ComputerSelection == "Paper")
+    else if(playerSelection == "Paper" && ComputerSelection == "Paper")
         return "even";
 
     if(playerSelection == "Scissors" && ComputerSelection == "Paper")
         return "You Win! Scissors beats Paper";
-    if(playerSelection == "Scissors" && ComputerSelection == "Rock")
+    else if(playerSelection == "Scissors" && ComputerSelection == "Rock")
         return "You Lose! Rock beats Scissors";
-    if(playerSelection == "Scissors" && ComputerSelection == "Scissors")
+    else if(playerSelection == "Scissors" && ComputerSelection == "Scissors")
         return "even";
 }
 
+function game(rounds){
+    
+    let playerWinningsCount = 0;
+    let computerWinningsCount = 0;
+    
+    console.log("Player: "+playerWinningsCount+" | Computer: " + computerWinningsCount+"");
+
+    do {
+        let ComputerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, ComputerSelection))
+        let result = playRound(playerSelection, ComputerSelection);
+        
+        if(result.includes("Win")) playerWinningsCount++;
+        else if(result.includes("Lose")) computerWinningsCount++;
+        
+        console.log("Player: "+playerWinningsCount+" | Computer: " + computerWinningsCount+"");
+    } while (playerWinningsCount <  rounds  &&
+         computerWinningsCount < rounds);
+    
+    if(playerWinningsCount > computerWinningsCount) console.log("You Win the game!!");
+    else console.log("You Lose. luck next time :)");
+}
+
 const playerSelection = "Rock";
-const ComputerSelection = getComputerChoice();
-console.log(playRound(playerSelection, ComputerSelection))
+game(2);
