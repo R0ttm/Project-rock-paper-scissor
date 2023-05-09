ed198729f181d1a25dd9cfe0d1aa29229a03ee3f
R0ttm <r0t7.mi@gmail.com>
2023-05-09 00:39:24 -0400

    feat: function getComputerChoice has been implemented.

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice(){
+    let randomNumber =Math.floor(Math.random() *3);
+    if(randomNumber === 0) return "Rock";
+    else if(randomNumber === 1) return "Paper";
+    else if(randomNumber === 2) return "Scissors";
+    else return "Error -  return out of range!!!";
+}
